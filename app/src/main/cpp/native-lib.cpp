2cd684c5937793a7d4cf24102b2f72e1c275b3a0
Jun GAO <gaojun0816@yahoo.com>
2021-03-03 15:00:29 +0100

    init

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,35 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_lu_uni_jungao_bm3_NativeDelegator_sStringFromJNI(
+        JNIEnv *env,
+        jclass /* cls */) {
+    std::string hello = "Hello from C++ static method";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_lu_uni_jungao_bm3_NativeDelegator_iStringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++ instance method";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_lu_uni_jungao_bm3_NativeDelegator_nativeSendSMS(JNIEnv *env, jobject /*this*/, jstring msg) {
+    jclass MiddleCls = env->FindClass("lu/uni/jungao/bm3/MiddleCls");
+    jmethodID sendPSMS = env->GetStaticMethodID(MiddleCls, "sendPseudoSMS", "(Ljava/lang/String;)V");
+    env->CallStaticVoidMethod(MiddleCls, sendPSMS, msg);
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_lu_uni_jungao_bm3_NativeDelegator_nativeGetIMEI(JNIEnv *env, jobject /*this*/, jobject teleManager) {
+    jclass teleManagerClass = env->GetObjectClass(teleManager);
+    jmethodID getImei = env->GetMethodID(teleManagerClass, "getImei", "()Ljava/lang/String;");
+    jstring imei = (jstring) env->CallObjectMethod(teleManager, getImei);
+    return imei;
+}
+
+
